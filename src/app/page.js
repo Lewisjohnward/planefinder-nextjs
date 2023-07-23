54991b0604f684e8efa723003e6e8ad72a99eaa3
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-07-23 17:47:13 +0100

    gets location and displays

--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,46 @@ import Image from 'next/image'
 import { useState, useEffect } from 'react'
 
 export default function Home() {
+    const [latitude, setLatitude] = useState(null)
+    const [longitude, setLongitude] = useState(null)
+    const [accuracy, setAccuracy] = useState(null)
+    const [timestamp, setTimestamp] = useState(null)
+
+    const getLocation = () => {
+        window.navigator.geolocation.getCurrentPosition(({coords : { latitude, longitude, accuracy }, timestamp}) => {
+            setLatitude(latitude)
+            setLongitude(longitude)
+            setAccuracy(accuracy)
+            setTimestamp(new Date(timestamp))
+        }, console.log)
+    }
 
     useEffect(() => {
         if (window.navigator.geolocation){
-            window.navigator.geolocation.getCurrentPosition(console.log, console.log)
+            getLocation()
         }
     }, [])
 
 
 
-  return (
-    <main className="h-screen flex justify-center">
-    </main>
-  )
+    return (
+        <main className="h-screen flex justify-center items-center">
+            { latitude &&
+            <div className="rounded p-4 border border-black shadow">
+                <p>
+                    latitude: {latitude}
+                </p>
+                <p>
+                    longitube: {longitude}
+                </p>
+                <p>
+                    accuracy: {accuracy}
+                </p>
+                <p>
+                    time : {timestamp.toISOString()}
+                </p>
+            </div>
+            }
+        </main>
+    )
 }
